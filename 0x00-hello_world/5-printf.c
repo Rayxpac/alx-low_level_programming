e39aab137c43e913ba47f265a171b03701dfc6c6
Rayxapc <godwinoderay@gmail.com>
2023-12-30 12:55:28 +0100

    program that print hello-world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - "prints the phrase with proper grammar,
+ *  but the outcome is a piece of art," follow by the new line
+ *  Return: Alway 0 follow with (sucess)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
